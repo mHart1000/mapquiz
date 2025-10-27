3000602081a2055c5d1f784e65d20a1cab266557
Matthew Hart <mhart1000@users.noreply.github.com>
2025-10-27 00:51:53 -0700

    add allowed hosts & env variables

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +28,7 @@ SECRET_KEY = 'django-insecure-u7#^#uud-tqlncl!i5fe)gsdh+6!xfo=3i#o_w(7f^e-2o-f9k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 # Application definition
 
