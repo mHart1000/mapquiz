e3116da4adf6de116fcd64a1507899a9480e199c
Matthew Hart <mhart1000@users.noreply.github.com>
2025-10-31 23:52:27 -0700

    quiz geo migration

--- backend/quizzes/models.py
+++ backend/quizzes/models.py
@@ -1,11 +1,10 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 class Quiz(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    geometry = models.MultiPolygonField(srid=4326)
-
+    geometry = models.MultiPolygonField(srid=4326, null=True, blank=True)
 
     def __str__(self):
         return self.name
