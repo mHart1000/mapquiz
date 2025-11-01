ae667b7741c2f8acca86aa19d1b279085177d009
Matthew Hart <mhart1000@users.noreply.github.com>
2025-10-31 23:47:59 -0700

    update models for postgis

--- backend/quizzes/models.py
+++ backend/quizzes/models.py
@@ -4,6 +4,8 @@ class Quiz(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    geometry = models.MultiPolygonField(srid=4326)
+
 
     def __str__(self):
         return self.name
